e05a30bf889c627ed4b1de0164986242b0975c04
sungwon_yun <sungwony0906@gmail.com>
2019-12-22 14:39:02 +0900

    PostsRepository 테스트코드 작성

--- src/test/java/com/sungwony/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sungwony/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -21,7 +25,22 @@ public class PostsRepositoryTest {
 
     @Test
     public void 게시글저장_불러오기(){
+        //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                                .title(title)
+                                .content(content)
+                                .author("sungwony0906@gmail.com")
+                                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
     }
 }
