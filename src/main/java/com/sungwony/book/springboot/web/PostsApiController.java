713b61da66a57a36a5c6b8ec0dff1051fcdb1292
sungwon_yun <sungwony0906@gmail.com>
2019-12-23 23:24:45 +0900

    Posts 엔티티 수정기능 및 테스트코드 구현

--- src/main/java/com/sungwony/book/springboot/web/PostsApiController.java
+++ src/main/java/com/sungwony/book/springboot/web/PostsApiController.java
@@ -1,12 +1,11 @@
 package com.sungwony.book.springboot.web;
 
 import com.sungwony.book.springboot.service.posts.PostsService;
+import com.sungwony.book.springboot.web.dto.PostsResponseDto;
 import com.sungwony.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sungwony.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +17,17 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
 
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+
+        return postsService.findById(id);
     }
 }
--- src/test/java/com/sungwony/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/sungwony/book/springboot/web/PostsApiControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sungwony.book.springboot.domain.posts.Posts;
 import com.sungwony.book.springboot.domain.posts.PostsRepository;
 import com.sungwony.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sungwony.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,6 +23,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -79,5 +79,34 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception{
+        //given
+        String title = "title";
+        String content = "content";
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .build());
+        //when
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                                                .title(expectedTitle)
+                                                .content(expectedContent)
+                                                .build();
+
+        String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+        //then
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
