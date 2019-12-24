f36532f368e8c2293012c0c9df4e427e97913074
sungwon_yun <sungwony0906@gmail.com>
2019-12-25 00:43:19 +0900

    BaseTimeEntity 생성
    createdDate, modifiedDate 관리
    및 테스트코드 추가

---
+++ src/main/java/com/sungwony/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,29 @@
+package com.sungwony.book.springboot.web.dto;
+
+import com.sungwony.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
